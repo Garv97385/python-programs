52f7e30e3718f9d2cb92cd805fdf9098e3966d19
Garv97385 <gs6859105@gmail.com>
2026-04-01 14:56:46 +0530

    Add files via upload

---
+++ # TRAFFIC LIGHTS.py
@@ -0,0 +1,10 @@
+# TRAFFIC LIGHTS 
+
+light = input("what is the traffic light color?  : ")
+
+if light == "green":
+    print("go")     
+elif light == "yellow":
+    print("slow down")
+elif light == "red":
+    print("stop")
